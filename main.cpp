6869ee15d24ff81e279466cf7985cacd3e54c6b9
shinny-tanyan <tanyan@shinnytech.com>
2023-11-30 10:19:18 +0800

    cache: push again

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char* argv[])
 {
     std::cout << "Hello Ccache" << std::endl;
     std::cout << "Hello World" << std::endl;
+    std::cout << "Hello Apt Cache" << std::endl;
     return 0;
 }
